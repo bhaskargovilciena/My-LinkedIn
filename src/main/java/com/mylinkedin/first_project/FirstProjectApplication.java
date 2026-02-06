75cd0ad7d04922a724594e0a4a73793cc0dbe6a1
bgovil <bgovil@ciena.com>
2026-02-06 15:08:55 +0530

    neo4j configuration successful

--- src/main/java/com/mylinkedin/first_project/FirstProjectApplication.java
+++ src/main/java/com/mylinkedin/first_project/FirstProjectApplication.java
@@ -2,11 +2,15 @@ package com.mylinkedin.first_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
-@SpringBootApplication
+@EnableNeo4jRepositories(basePackages = "com.mylinkedin.first_project")
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class FirstProjectApplication {
 
 	public static void main(String[] args) {
+		System.out.println("AURADB_URI: " + System.getenv("AURADB_URI"));
 		SpringApplication.run(FirstProjectApplication.class, args);
 	}
 
